3e7a68fe12c29792d5ce4a965bef3c097598cfac
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-08 21:16:36 +0900

    feat: add `Dev` page structures and styles
    #38

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -1,146 +1,87 @@
-import ContentsListElement from 'components/ContentsListElement';
 import Layout from 'components/Layout';
-import { graphql } from 'gatsby';
-import React, { useMemo, useState } from 'react';
+import * as React from 'react';
 
 import './dev.scss';
 
-export type Post = {
-  node: {
-    id: 'string';
-    slug: 'string';
-    frontmatter: {
-      date: 'string';
-      tags: string[];
-      title: 'string';
-    };
-    body: 'string';
-  };
-};
-
-export type CategoryType = {
-  fieldValue: 'string';
-  edges: [Post];
-};
-
-function DevPage({ data }: any) {
-  const [categorySelected, setCategorySelected] = useState('Front');
-  const [tagSelected, setTagSelected] = useState('전체');
-  const categoryGroup = data.allMdx.group;
-  const [tagGroup, setTagGroup] = useState<JSX.Element[]>([]);
-  const [currentPostList, setCurrentPostList] = useState<JSX.Element[]>([]);
-
-  useMemo(() => {
-    categoryGroup.map((category: any) => {
-      if (category.fieldValue === categorySelected) {
-        setTagGroup(category.group);
-      }
-    });
-  }, [categorySelected]);
-
-  useMemo(() => {
-    tagGroup.map((tag: any) => {
-      if (tagSelected === '전체') {
-        setCurrentPostList(tag.nodes);
-      } else if (tag.fieldValue === tagSelected) {
-        setCurrentPostList(tag.nodes);
-      }
-    });
-  }, [tagGroup, tagSelected]);
-
+function DevPage() {
   return (
     <Layout>
       <div className="dev-page">
-        <section className="title-wrap">
-          <span className="title">Dev</span>
-          {categoryGroup.map((category: any, index: number) => {
-            return (
-              <div
-                key={category.fieldValue + index}
-                className={
-                  category.fieldValue === categorySelected
-                    ? 'category underline'
-                    : 'category'
-                }
-                onClick={() => {
-                  setCategorySelected(category.fieldValue);
-                  setTagSelected('전체');
-                }}>
-                {category.fieldValue}
-              </div>
-            );
-          })}
-        </section>
-
-        <section className="tag-wrap">
-          <div
-            className={tagSelected === '전체' ? 'tag selected' : 'tag all'}
-            onClick={() => {
-              setTagSelected('전체');
-            }}>
-            전체
+        <article className="categories">
+          <section className="category">
+            <li className="clicked">Frontend</li>
+            <li>Backend</li>
+            <li>CS</li>
+            <li>Git/Github</li>
+            <li>Activity</li>
+            <li>Project</li>
+          </section>
+          <section className="subcategory">
+            <li className="all clicked">All</li>
+            <li className="clicked">JavaScript</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>React</li>
+            <li>React</li>
+            <li>React</li>
+            <li>React</li>
+            <li>React</li>
+            <li>React</li>
+            <li>React</li>
+          </section>
+        </article>
+        <article className="contents">
+          <div className="content">
+            <img src="" />
+            <div className="title">title이 들어갈 자리</div>
+            <div className="date">2022.04.08</div>
           </div>
-          {tagGroup.map((tag: any, index) => {
-            return (
-              <div
-                className={
-                  tag.fieldValue === tagSelected ? 'tag selected' : 'tag'
-                }
-                key={index}
-                onClick={() => {
-                  setTagSelected(tag.fieldValue);
-                }}>
-                {tag.fieldValue}
-              </div>
-            );
-          })}
-        </section>
-
-        <section className="contents-list">
-          {currentPostList.map((post: any) => {
-            return (
-              <ContentsListElement
-                key={post.id}
-                tags={post.frontmatter.tags}
-                title={post.frontmatter.title}
-                contents={post.excerpt}
-                date={post.frontmatter.date}
-                slug={post.slug}
-                mainImage={post.frontmatter.main_image}
-              />
-            );
-          })}
-        </section>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+        </article>
       </div>
     </Layout>
   );
 }
-export const query = graphql`
-  query {
-    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-      group(field: frontmatter___category) {
-        fieldValue
-        group(field: frontmatter___tags) {
-          fieldValue
-          nodes {
-            frontmatter {
-              date(formatString: "YYYY.MM.DD")
-              tags
-              title
-              main_image {
-                childImageSharp {
-                  gatsbyImageData
-                }
-              }
-            }
-            id
-            slug
-            excerpt(truncate: true, pruneLength: 30)
-          }
-        }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query {
+//     allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+//       group(field: frontmatter___category) {
+//         fieldValue
+//         group(field: frontmatter___tags) {
+//           fieldValue
+//           nodes {
+//             frontmatter {
+//               date(formatString: "YYYY.MM.DD")
+//               tags
+//               title
+//               main_image {
+//                 childImageSharp {
+//                   gatsbyImageData
+//                 }
+//               }
+//             }
+//             id
+//             slug
+//             excerpt(truncate: true, pruneLength: 30)
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
 
 export default DevPage;
