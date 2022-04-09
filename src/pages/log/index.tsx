692e7251c6485a86f8fdee3020f9a3a62eb11905
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-09 11:49:54 +0900

    feat: add `Log` page structures and styles
    #38]

--- src/pages/log/index.tsx
+++ src/pages/log/index.tsx
@@ -1,12 +1,44 @@
 import Layout from 'components/Layout';
 import * as React from 'react';
-import 'styles/index.scss';
+import 'styles/page.scss';
 
 function LogPage() {
   return (
     <Layout>
-      <div>Log Page</div>
-      <div>개발중</div>
+      <div className="page">
+        <article className="categories">
+          <section className="category">
+            <li className="clicked">Frontend</li>
+            <li>Backend</li>
+            <li>CS</li>
+            <li>Git/Github</li>
+            <li>Activity</li>
+            <li>Project</li>
+          </section>
+        </article>
+        <article className="contents">
+          <div className="content">
+            <img src="" />
+            <div className="title">title이 들어갈 자리</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+        </article>
+      </div>
     </Layout>
   );
 }
