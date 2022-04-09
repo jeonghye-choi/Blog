12d7f6f777f11201b8046d6e6ca38237fd4de167
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-09 11:48:49 +0900

    feat: add Write page structrues and styles
    #38

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,12 +1,34 @@
 import Layout from 'components/Layout';
 import * as React from 'react';
-import 'styles/index.scss';
+import 'styles/page.scss';
 
 function WritePage() {
   return (
     <Layout>
-      <div>Write Page</div>
-      <div>개발중</div>
+      <div className="page">
+        <article className="contents">
+          <div className="content">
+            <img src="" />
+            <div className="title">title이 들어갈 자리</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+          <div className="content">
+            <img src="" />
+            <div className="title">title</div>
+            <div className="date">2022.04.08</div>
+          </div>
+        </article>
+      </div>
     </Layout>
   );
 }
