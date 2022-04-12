60fbb515984f4057701eb3f6d037a706f73dbd1d
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-12 23:42:32 +0900

    feat: add `Tag` component
    #41

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import './tag.scss';
+
+function Tag() {
+  return (
+    <section className="tag">
+      <li className="all clicked">All</li>
+      <li className="clicked">JavaScript</li>
+      <li>TypeScript</li>
+      <li>React</li>
+      <li>React</li>
+      <li>React</li>
+      <li>React</li>
+      <li>React</li>
+      <li>React</li>
+      <li>React</li>
+      <li>React</li>
+    </section>
+  );
+}
+
+export default Tag;
