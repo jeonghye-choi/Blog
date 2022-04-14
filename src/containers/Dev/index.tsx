2c99c4ae73e506d9a6fa9570cc91755223486c5d
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-14 20:31:51 +0900

    feat: add actions for data with graphQL at Dev Page
    #45

--- src/containers/Dev/index.tsx
+++ src/containers/Dev/index.tsx
@@ -1,15 +1,20 @@
 import Category from 'components/Category';
 import Tag from 'components/Tag';
+import usePostsActions from 'hooks/usePostsActions';
 import * as React from 'react';
 import 'styles/page.scss';
 
 import DevContent from './DevContent';
 
 function Dev() {
+  const postActions = usePostsActions();
+  const devPosts = postActions.getPosts().posts.edges;
+
   return (
     <>
       <Category />
       <Tag />
+      <DevContent posts={devPosts} />
     </>
   );
 }
