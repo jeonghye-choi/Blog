267cc76fb6b40b1d269ea90acb80673db677d21e
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-20 19:26:50 +0900

    feat: add posts filtered by tags in Log page
    #53

--- src/containers/Log/LogContent/index.tsx
+++ src/containers/Log/LogContent/index.tsx
@@ -1,13 +1,38 @@
+import { PostsEdgeType } from 'CreatePostPagesQuery';
 import ListCard from 'components/ListCard';
 import * as React from 'react';
+import { useMemo } from 'react';
 
 import './logContent.scss';
 
-function LogContent() {
+interface LogContentProps {
+  posts: PostsEdgeType[];
+  selectedTag: string;
+}
+
+function LogContent(props: LogContentProps) {
+  const { posts, selectedTag } = props;
+
+  const filteredPosts = useMemo(
+    () =>
+      posts.filter(
+        ({
+          node: {
+            frontmatter: { tags },
+          },
+        }: PostsEdgeType) =>
+          selectedTag != 'All' ? tags?.includes(selectedTag) : true,
+      ),
+    [selectedTag],
+  );
+
   return (
     <section className="content">
-      <ListCard />
-      <ListCard />
+      {filteredPosts.map(
+        ({ node: { id, frontmatter, slug } }: PostsEdgeType) => (
+          <ListCard key={id} id={id} link={slug} {...frontmatter} />
+        ),
+      )}
     </section>
   );
 }
