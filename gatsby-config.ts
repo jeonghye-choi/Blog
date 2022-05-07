4d1836f4297863d027f90c717106d8fc36ac6164
jeonghye-choi <ppllhm.0@gmail.com>
2022-05-07 23:56:10 +0900

    feat: add `prismjs` remark styles
    #57

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -31,6 +31,29 @@ module.exports = {
       options: {
         gatsbyRemarkPlugins: [
           {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: { sh: 'bash' },
+              showLineNumbers: false,
+              numberLines: true,
+              noInlineHighlight: false,
+              // Customize the prompt used in shell output
+              // Values below are default
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: false,
+              },
+              // By default the HTML entities <>&'" are escaped.
+              // Add additional HTML escapes by providing a mapping
+              // of HTML entities and their escape value IE: { '}': '&#123;' }
+              escapeEntities: {},
+            },
+          },
+
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 500,
