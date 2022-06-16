732a4321ae10d9c4a62c4c438f6662f922e7a95f
jeonghye-choi <ppllhm.0@gmail.com>
2022-06-17 00:28:42 +0900

    feat(index): add career list in home page
    #73

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -13,6 +13,31 @@ function Home() {
           최정혜입니다.
         </p>
       </section>
+      <section className="home-work">
+        <h3>I&apos;ve worked at</h3>
+        <ul>
+          <li>
+            <span className="left">Viva Republica (Toss)</span>
+            <span className="center">UX Engineer (Assistant)</span>
+            <span className="right">2022.6.27 - Present</span>
+          </li>
+          <li>
+            <span className="left">HoneyBees</span>
+            <span className="center">FE Developer</span>
+            <span className="right">2021.6 - Present</span>
+          </li>
+          <li>
+            <span className="left">ASSI (Readers)</span>
+            <span className="center">FE Intern</span>
+            <span className="right">2020.10</span>
+          </li>
+          <li>
+            <span className="left">While True (OnAd)</span>
+            <span className="center">UI/UX Designer</span>
+            <span className="right">2019.5.3 - 2019.9.13</span>
+          </li>
+        </ul>
+      </section>
     </article>
   );
 }
