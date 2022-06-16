a0fff8d495a63d7c93decfe7f209e902b706a63a
jeonghye-choi <ppllhm.0@gmail.com>
2022-06-16 23:57:07 +0900

    feat(index): add introductions in home page
    #73

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,20 @@
-import Layout from 'components/Layout';
 import * as React from 'react';
 
+import './home.scss';
+
 function Home() {
-  return <Layout page="Index">index Page</Layout>;
+  return (
+    <article id="home">
+      <section className="home-introduction">
+        <h2>Hi Guys, I&apos;m jeonghye</h2>
+        <p className="en">I’m a Web Frontend Developer.</p>
+        <p className="kr">
+          성장을 위해 도전하고, 성공을 위해 시스템을 만드는 프런트 개발자,
+          최정혜입니다.
+        </p>
+      </section>
+    </article>
+  );
 }
 
 export default Home;
